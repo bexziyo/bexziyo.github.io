f23fc7e5be7edb290daa2457a819d6dd9a565e02
bexziyo <bex.ziyo@mail.ru>
2024-02-26 23:19:18 +0500

    to top

--- js/script.js
+++ js/script.js
@@ -19,35 +19,24 @@ menuBtn.addEventListener("click", () => {
 
 // **************************************************************************************
 
-// const openModal1 = document.getElementById('openModal1')
-// const dialog1 = document.getElementById('dialog1')
-// const closeModal1 = document.getElementById('closeModal1')
-
-// openModal1.addEventListener("click", () => {
-//   dialog1.classList.add("open-modal");
-// });
-// closeModal1.addEventListener("click", () => {
-//   dialog1.classList.remove("open-modal");
-// });
-
 let openModalBtn = document.getElementById("openModalBtn");
 let modal = document.getElementById("myModal");
 let closeModalBtn = document.getElementById("closeModalBtn");
 
-openModalBtn.addEventListener("click", function() {
-    modal.style.display = "block";
+openModalBtn.addEventListener("click", function () {
+  modal.style.display = "block";
 
 });
 
-closeModalBtn.addEventListener("click", function() {
+closeModalBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
     modal.style.display = "none";
-
-});
-
-window.addEventListener("click", function(event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
+  }
 });
 
 
@@ -58,13 +47,13 @@ const marqueeContent = document.querySelector("ul.marquee-content");
 
 root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 
-for(let i=0; i<marqueeElementsDisplayed; i++) {
+for (let i = 0; i < marqueeElementsDisplayed; i++) {
   marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
 }
 // **************************************************************************************
 
 
-function progressBarAndCountNumber () {
+function progressBarAndCountNumber() {
 
   const progress = document.querySelectorAll('.progress');
   let count = 0;
@@ -72,20 +61,62 @@ function progressBarAndCountNumber () {
   let MAX = 100;
 
   let run = setInterval(() => {
-      count++;
-      progress.forEach(element => {
-          if (count <= element.dataset.progress) {
-              element.parentElement.style.background = `conic-gradient(#5c27fe ${count}%, #212428 0)`;
-              element.firstElementChild.textContent = `${count}%`;
-          };
-          if (count == MAX) {
-              clearInterval(run);
-          };
-      });
+    count++;
+    progress.forEach(element => {
+      if (count <= element.dataset.progress) {
+        element.parentElement.style.background = `conic-gradient(#5c27fe ${count}%, #212428 0)`;
+        element.firstElementChild.textContent = `${count}%`;
+      };
+      if (count == MAX) {
+        clearInterval(run);
+      };
+    });
   }, 50);
 }
 
-setTimeout( function() { progressBarAndCountNumber();; }, 4000);
+setTimeout(function () { progressBarAndCountNumber();; }, 4000);
 
 
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+      var scroll = window.pageYOffset || document.documentElement.scrollTop;
+
+      if (scroll > 200) {
+          document.getElementById('top').style.display = "block";
+      } else {
+          document.getElementById('top').style.display = "none";
+      }
+  });
+
+  document.getElementById('top').addEventListener("click", function () {
+      scrollToTop(1000);
+  });
+});
+
+function scrollToTop(duration) {
+  var start = window.pageYOffset || document.documentElement.scrollTop,
+      currentTime = 0,
+      increment = 20;
+
+  var animateScroll = function () {
+      currentTime += increment;
+      var val = Math.easeInOutQuad(currentTime, start, -start, duration);
+      window.scrollTo(0, val);
+      if (currentTime < duration) {
+          requestAnimationFrame(animateScroll);
+      }
+  };
+  animateScroll();
+}
+
+Math.easeInOutQuad = function (t, b, c, d) {
+  t /= d / 2;
+  if (t < 1) return c / 2 * t * t + b;
+  t--;
+  return -c / 2 * (t * (t - 2) - 1) + b;
+};
 
